85155f28ad443931a6b791c4f42cc8adc45768cf
Sergey Poskachey <sepo@one.com>
2019-06-13 15:09:34 +0400

    Webpack init

---
+++ webpack/webpack.config.js
@@ -0,0 +1,76 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const { ppath } = require('./webpackUtils');
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    main: ppath.toSrc(),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          ppath.toSrc(),
+        ],
+        use: {
+          loader: 'babel-loader',
+          // options will be taken from .babelrc
+        },
+      },
+    ],
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          name: 'vendor',
+          priority: -10,
+        },
+      },
+    },
+  },
+
+  output: {
+    path: ppath.toDist('bundle'),
+    publicPath: '/bundle/',
+    filename: '[name].js',
+    pathinfo: true, // dump path info with each webpack require in bundle
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      alwaysWriteToDisk: true, // for wbpack-dev-server to generate index.html on the fly
+      title: 'Wsb data flow research app',
+      filename: ppath.toDist('index.html'),
+      template: ppath.toSrc('index.template.html'),
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    // new webpack.HotModuleReplacementPlugin()
+  ],
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+
+  stats: {
+    children: false,
+    version: false,
+    maxModules: 0, // hide modules (https://github.com/webpack/webpack/issues/4141)
+  },
+
+  devServer: {
+    contentBase: ppath.toDist(),
+    compress: true,
+    // hotOnly: true,
+    port: 3200,
+    stats: 'minimal',
+  },
+};
---
+++ webpack/webpackUtils.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const ROOT = `${__dirname}/../`;
+
+const ppath = {
+  to(...segments) { return path.resolve(path.normalize(ROOT), ...segments); },
+  toSrc(segment = '') { return this.to('src', segment); },
+  toDist(segment = '') { return this.to('dist', segment); },
+  toTest(segment = '') { return this.to('test', segment); },
+  toModules(segment = '') { return this.to('node_modules', segment); },
+};
+
+module.exports = {
+  ppath,
+};
