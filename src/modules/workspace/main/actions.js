6d851f0295de76ff8ec8f263caf65a6564f60a86
Sergey Poskachey <seregynp@gmail.com>
2019-06-29 13:52:38 +0400

    Refactor

--- src/modules/workspace/main/actions.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-
-export const
-  WORKSPACE_INITIALIZED_ACTION = 'WORKSPACE_INITIALIZED_ACTION',
-  workspaceInitializedAction = () => ({
-    type: WORKSPACE_INITIALIZED_ACTION,
-  });
