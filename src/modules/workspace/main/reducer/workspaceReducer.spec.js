1b48fc5019f128a6586daed1abba01334597dd63
Sergey Poskachey <sepo@one.com>
2019-06-30 10:37:12 +0400

    Increasing margin width by moving component to right edge

--- src/modules/workspace/main/reducer/workspaceReducer.spec.js
+++ src/modules/workspace/main/reducer/workspaceReducer.spec.js
@@ -57,4 +57,50 @@ describe('workspaceReducer', () => {
       },
     });
   });
+
+  it('moves margin by right edge along with components when not locked', () => {
+    const
+      com = baseComponentStateGen(ComponentKind.BUTTON, {
+        id: '321',
+        position: {
+          top: 70,
+          left: 1200,
+        },
+        dimensions: {
+          height: 40,
+          width: 50,
+        },
+      }),
+      deps = {
+        wsWidth: 1700,
+        margin: {isLocked: true, width: 1000},
+      },
+      action = moveComponentAction('321', {
+        top: 77,
+        left: 1400,
+      });
+
+    let state = rAssocPath([Lit.width], 1700, WorkspaceInitialState);
+    state = rAssocPath([Lit.margin, Lit.isLocked], false, state);
+    state = rAssocPath([Lit.margin, Lit.width], 1000, state);
+    state = rAssocPath([Lit.components], [com], state);
+
+    expect(reducer(state, action, deps)).toEqual({
+      ...WorkspaceInitialState,
+      components: [
+        {
+          ...com,
+          position: {
+            top: 77,
+            left: 1400,
+          },
+        },
+      ],
+      width: 1700,
+      margin: {
+        ...WorkspaceInitialState.margin,
+        width: 1200,
+      },
+    });
+  });
 });
