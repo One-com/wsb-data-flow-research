1b1ba5e98079e8770b31ea0a8c190bfd64dcee4f
Sergey Poskachey <seregynp@gmail.com>
2019-07-09 22:43:37 +0400

    Fix save button

--- src/modules/save/status/saveStatusReducer.js
+++ src/modules/save/status/saveStatusReducer.js
@@ -4,7 +4,7 @@ import {equals as rEquals} from 'ramda';
 import type { SaveStatusDependencies, SaveStatusState } from './types';
 import { SaveStatus } from './constants';
 import { getSavableAppState } from '../main/getSavableAppState';
-import { PUT_APP_STATE_SUCCESS_ACTION, SAVE_ACTION } from '../main/actions';
+import { FETCH_APP_STATE_SUCCESS_ACTION, PUT_APP_STATE_SUCCESS_ACTION, SAVE_ACTION } from '../main/actions';
 
 export const SaveStatusInitialState: SaveStatusState = SaveStatus.SAVED;
 
@@ -20,6 +20,7 @@ export const saveStatusReducer = (
   if (
     Object.keys(prevAppState).length
     && !rEquals(getSavableAppState(prevAppState), getSavableAppState(nextAppState))
+    && action.type !== FETCH_APP_STATE_SUCCESS_ACTION
   ) {
     return SaveStatus.UNSAVED;
   } else if (action.type === PUT_APP_STATE_SUCCESS_ACTION) {
